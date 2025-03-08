31be7ddd0faace69f950f3415a901bee9875bfdf
Mohammad Al Samiul <alsamiul123@gmail.com>
2025-03-08 09:11:02 +0600

    user authentication completed with clerk

--- src/pages/shared/navbar.tsx
+++ src/pages/shared/navbar.tsx
@@ -1,16 +1,26 @@
 import { Button } from "@/components/ui/button";
 import { SignedIn, SignedOut, SignIn, UserButton } from "@clerk/clerk-react";
-import { PenBox } from "lucide-react";
-import { useState } from "react";
-import { Link } from "react-router-dom";
+import { BriefcaseBusiness, Heart, PenBox } from "lucide-react";
+import { useEffect, useState } from "react";
+import { Link, useSearchParams } from "react-router-dom";
 
 export const Navbar = () => {
   const [showSignIn, setShowSignIn] = useState(false);
+  const [search, setSearch] = useSearchParams();
+
+  useEffect(() => {
+    if (search.get("sign-in") === "true") {
+      setShowSignIn(true);
+    }
+  }, [search]);
+
   const handleOverlayClick = (e: React.MouseEvent<HTMLDivElement>) => {
     if (e.target === e.currentTarget) {
       setShowSignIn(false);
+      setSearch({});
     }
   };
+
   return (
     <>
       <nav className="flex justify-between items-center py-4 ">
@@ -25,13 +35,33 @@ export const Navbar = () => {
             </Button>
           </SignedOut>
           <SignedIn>
-            <UserButton />
             <Link to="/post-job">
               <Button variant="destructive" className="rounded-full">
                 <PenBox size={20} className="mr-2" />
                 Post a Job
               </Button>
             </Link>
+            <UserButton
+              appearance={{
+                elements: {
+                  avatarBox: "w-10 h-10",
+                },
+              }}
+            >
+              <UserButton.MenuItems>
+                <UserButton.Link
+                  label="My Jobs"
+                  labelIcon={<BriefcaseBusiness size={15} />}
+                  href="/my-jobs"
+                />
+                <UserButton.Link
+                  label="Saved Jobs"
+                  labelIcon={<Heart size={15} />}
+                  href="/saved-jobs"
+                />
+                <UserButton.Action label="manageAccount" />
+              </UserButton.MenuItems>
+            </UserButton>
           </SignedIn>
         </div>
       </nav>
