a1f4bb9c8303a83582c2a95eb1b8eb75c8c9a4e2
Mohammad Al Samiul <alsamiul123@gmail.com>
2025-03-08 07:19:36 +0600

    banner page added

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,8 +1,8 @@
-import * as React from "react"
-import { Slot } from "@radix-ui/react-slot"
-import { cva, type VariantProps } from "class-variance-authority"
+import * as React from "react";
+import { Slot } from "@radix-ui/react-slot";
+import { cva, type VariantProps } from "class-variance-authority";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
   "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
@@ -18,12 +18,14 @@ const buttonVariants = cva(
         secondary:
           "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
+        blue: "bg-blue-500 text-white shadow-sm hover:bg-blue-600",
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
         default: "h-9 px-4 py-2",
         sm: "h-8 rounded-md px-3 text-xs",
         lg: "h-10 rounded-md px-8",
+        xl: "h-14 sm:h-16 rounded-md px-14 text-lg sm:text-xl font-bold",
         icon: "h-9 w-9",
       },
     },
@@ -32,26 +34,26 @@ const buttonVariants = cva(
       size: "default",
     },
   }
-)
+);
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
-  asChild?: boolean
+  asChild?: boolean;
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
-    const Comp = asChild ? Slot : "button"
+    const Comp = asChild ? Slot : "button";
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
         {...props}
       />
-    )
+    );
   }
-)
-Button.displayName = "Button"
+);
+Button.displayName = "Button";
 
-export { Button, buttonVariants }
+export { Button, buttonVariants };
--- src/pages/home/landing-page.tsx
+++ src/pages/home/landing-page.tsx
@@ -1,6 +1,9 @@
+import { Button } from "@/components/ui/button";
+import { Link } from "react-router-dom";
+
 export const LandingPage = () => {
   return (
-    <main>
+    <main className="flex flex-col gap-10 sm:gap-20 py-10 sm:py-20">
       <section className="text-center ">
         <h1 className="flex flex-col items-center justify-center gradient-title font-extrabold text-4xl sm:text-6xl lg:text-8xl tracking-tighter py-4">
           Find Your Dream Job
@@ -17,6 +20,18 @@ export const LandingPage = () => {
           Explore thousands of job listings or find the perfect candidate
         </p>
       </section>
+      <div className="flex gap-6 justify-center">
+        <Link to={"/jobs"}>
+          <Button variant="blue" size="xl">
+            Find Jobs
+          </Button>
+        </Link>
+        <Link to={"/post-job"}>
+          <Button variant="destructive" size="xl">
+            Post a Job
+          </Button>
+        </Link>
+      </div>
     </main>
   );
 };
